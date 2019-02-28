bf785dd842fca655f6fa38029bded28b67755f5a
NiranDx <niran3688@gmail.com>
2019-02-28 16:55:02 +0700

    add express secssion

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 var express = require('express'),
    app = express(),
    bodyParser = require('body-parser');
+var session = require('express-session');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 10000 },
+   resave : false, saveUninitialized: false }))
+
+app.use(function(req, res, next) {
+    var sess = req.session
+    next();
+ })
+ 
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.post('/admin', urlencodedParser, function(req, res){
-    if(req.body.user=='Niran' && req.body.pass=='1234'){
-        res.render('admin', {admins: ['Hello'] , foo: req.body.user, bt: 'Logout' })
+    req.session.user = req.body.user
+    req.session.pass = req.body.pass
+    if( req.session.user=='Niran' && req.session.pass=='1234'){
+        res.render('admin', {admins: ['Hello'] , foo:  req.session.user, bt: 'Logout' })
         
     }
     else {
         res.render('admin', {admins: ['Please login first'] , foo:'', bt: 'Login'})
     }
  })
+ app.get('/admin', urlencodedParser, function(req, res){ 
+    if( req.session.user=='Niran' && req.session.pass=='1234'){
+        res.render('admin', {admins: ['Hello'] , foo:  req.session.user, bt: 'Logout' })
+        
+    }
+    else {
+        res.render('admin', {admins: ['Please login first'] , foo:'', bt: 'Login'})
+    }
+ })
+
+ app.get('/logout', (req,res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/form.html');
+        }
+    });
+ });
+ 
 
 app.listen(8000);
